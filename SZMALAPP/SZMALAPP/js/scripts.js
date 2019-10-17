a8386ef6728efe5e3ff936106051ef9caf7c2136
Filip Woźniak <filip.wozniak@student.wat.edu.pl>
2019-10-17 18:31:39 +0200

    Commit inicjujący

---
+++ SZMALAPP/SZMALAPP/js/scripts.js
@@ -0,0 +1,16 @@
+function showRegisterPopup() {
+	document.getElementById('register-wrapper').style.display='block'
+}
+
+function showLoginPopup() {
+	document.getElementById('login-wrapper').style.display='block'
+}
+
+function showAboutPopup() {
+    document.getElementById('about-wrapper').style.display = 'block'
+}
+
+function openFacebook() {
+    var win = window.open("https://www.facebook.com/SZMALCompany", '_blank');
+    win.focus();
+}
