4cec6581f20c65742340d74ac8a0d12a437b6c56
Bartosz Konecki <bartosz.konecki@student.wat.edu.pl>
2019-10-18 09:33:30 +0200

    Prób MVC

---
+++ SZMALAPP/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SZMALAPP.Models;
+
+namespace SZMALAPP.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("~/Views/Index.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(uzytkownik objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (szmaldbEntities db = new szmaldbEntities())
+                {
+                    var obj = db.uzytkowniks.Where(a => a.imie.Equals(objUser.login) && a.haslo.Equals(objUser.haslo)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.login.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ SZMALAPP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SZMALAPP.MvcApplication" Language="C#" %>
