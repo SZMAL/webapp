1330661a68ed91ffe413c2e51c09b800258e804c
student <student@WIN_10_32>
2019-10-18 11:49:45 +0200

    Przekierowania

--- SZMALAPP/App_Start/RouteConfig.cs
+++ SZMALAPP/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SZMALAPP
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller="Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
