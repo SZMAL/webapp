56776d3c66bfe35e8f1069cb8e75c8bfd72eddbb
hasebedej <radoslaw.kister@student.wat.edu.pl>
2019-11-11 21:16:14 +0100

    Komentarze dla Bartka

--- SZMALAPP/Scripts/scripts.js
+++ SZMALAPP/Scripts/scripts.js
@@ -54,6 +54,15 @@ function showPosition(position) {
     var latSpan = northEast.lat() - southWest.lat();
     // Oznaczamy nasz punkt za pomoc¹ markera
     var iconBase = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
+
+
+
+
+    //!!!!!BARTEK!!!!!! daj mi tutaj jakiœ vector albo liste zg³oszeñ z bazy. Najlepiej tylko tych potwierdzonych
+
+
+
+
     for (var i = 1; i < 5; i++) {
         // init markers
         var marker = new google.maps.Marker({
@@ -125,6 +134,8 @@ function choosePlace(position) {
         }
         lat = marker.getPosition().lat();
         lng = marker.getPosition().lng();
+
+        ///!!!!!!BARTEK !!!!!! lat i lng to szerokosc i d³ugoœæ, trzeba to wprowadziæ do bazy
         var latlng = new google.maps.LatLng(lat, lng);
         geocoder.geocode({ 'latLng': latlng },
             function (results, status) {
@@ -132,6 +143,8 @@ function choosePlace(position) {
                     if (results[0]) {
                         infowindow.setContent(results[0].formatted_address);
                         infowindow.open(map, marker);
+                        //!!!!BARTEK!!!! result[0] to adres. Adres bedzie teraz jednym ciagiem stringów
+                        //nie bedziemy go rozbijac na ulice itp. trzeba to wprowadzic do bazy 
                     }
                     else {
                         alert('No results found');
@@ -143,4 +156,30 @@ function choosePlace(position) {
             });
         map.panTo(position);
     }
+}
+
+
+
+
+function showEventOnMap() {
+    //!!!!!BARTEK!!! Tutaj potrzebuje szerokosci i d³ugosci danego zdarzenia
+    var lat1 = 52.242399;
+    var lng1 = 20.874975;
+    var position = new google.maps.LatLng(lat, lng);
+    var image_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + " &zoom=14&key=AIzaSyDHwdYW-xzRa23-EuuiaKSqcoy2x7Lspwo";
+
+   
+    document.getElementById("right-box-map").innerHTML = "<img src='" + image_url + "'>";
+    var map = new google.maps.Map(
+        document.getElementById('right-box-map'), { zoom: 14, center: { lat: lat1, lng: lng1 }, mapTypeControl: false });
+   
+    var iconBase = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
+
+    marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: 'Ustaw to miejsce'
+    });
+ 
+
 }
